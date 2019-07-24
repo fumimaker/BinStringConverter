cc2dbcaa46897e56f128d89268f455c06443f209
fumimaker <fumi01200924@gmail.com>
2019-07-24 21:00:08 +0900

    なんか動いた

--- conv.c
+++ conv.c
@@ -1,34 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define array_num 131071
+
 long GetFileSize(FILE *fp);
-int main(void)
-{
-    FILE *fp = fopen("inuko9bit.raw", "rb");
-    if (fp == NULL){
-        fputs("ファイルオープンに失敗しました。\n", stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    
-    long fileSize = GetFileSize(fp); //77826
-    printf("%ld\n", fileSize);
-    unsigned char data[77826] = {};
-
-    if (fread(&data, sizeof(data), fileSize, fp) < 1){
-        fputs("読み込み中にエラーが発生しました。\n", stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    
-
-    if (fclose(fp) == EOF){
-        fputs("ファイルクローズに失敗しました。\n", stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    return 0;
-}
+unsigned char data[77826] = {};
+long fileSize;
 
 long GetFileSize(FILE *fp)
 {
@@ -44,3 +19,49 @@ long GetFileSize(FILE *fp)
 
     return size;
 }
+
+void conv(void)
+{
+    for (int i = 0; i < fileSize; i = i + 3)
+    {
+        unsigned char r = data[i] >> 5;
+        unsigned char g = data[i + 1] >> 5;
+        unsigned char b = data[i + 2] >> 5;
+        printf("%03X\n", (r << 6) | (g << 3) | (b << 0));
+    }
+}
+
+int main(void){
+    FILE *fp = fopen("inuko9bit.raw", "rb");
+    if (fp == NULL){
+        fputs("ファイルオープンに失敗しました。\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    
+    fileSize = GetFileSize(fp); //77826
+    printf("%ld\n", fileSize);
+    
+
+    if (fread(&data, sizeof(data), fileSize, fp) < 1){
+        fputs("読み込み中にエラーが発生しました。\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (fclose(fp) == EOF){
+        fputs("ファイルクローズに失敗しました。\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    conv();
+    return 0;
+}
+
+
+
+/*
+r=getchar()>>5;
+g=getchar()>>5;
+b=getchar()>>5;
+alpha=getchar();
+printf(“%03X¥n”, (r << 6) | (g << 3) | (b));
+ */
